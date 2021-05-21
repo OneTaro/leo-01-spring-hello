d2fa0e43d4d209fb9f2703b456113d7a6376f9c0
李白与孟浩然 <OneTaro@users.noreply.github.com>
2021-05-21 21:15:30 +0800

    spring项目

--- src/main/java/com/leo/spring/hello/Leo001SpringHelloApplication.java
+++ src/main/java/com/leo/spring/hello/Leo001SpringHelloApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Leo01SpringHelloApplication {
+public class Leo001SpringHelloApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Leo01SpringHelloApplication.class, args);
+        SpringApplication.run(Leo001SpringHelloApplication.class, args);
     }
 
 }
